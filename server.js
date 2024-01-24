be760c7051d62f96e26a2670d275f547b56da3d3
Shashank K R <krshashank1118@gmail.com>
2024-01-25 02:08:12 +0530

    Revert "Production ready Changes - static file in server.js"
    This reverts commit 3a9267fb2c2e93b2bad5d490293fe01759d22741.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const connectDb = require('./config/connectDB');
-const path = require('path');
 
 // dotenv config
 require('dotenv').config();
@@ -22,14 +21,6 @@ app.use(morgan('dev'));
 // routes
 app.use("/api/v1/users" , require("./routes/userRoute"));
 app.use("/api/v1/transactions" , require("./routes/transactionRoute"))
-
-// Static files - Production
-app.use(express.static(path.join(__dirname, './client/build')))
-
-app.get('*', function(req,res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
-
 
 const PORT = 8000 || process.env.PORT;
 
